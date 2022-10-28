9d115f3a60669d07dc38a5d725ef068c0f803ba6
kot_1999 <oleksandr.kashytskyi@goodreqeust.com>
2022-10-28 16:47:43 +0200

    OK-03:Script updated

--- scripts/movies_to_json.js
+++ scripts/movies_to_json.js
@@ -3,4 +3,4 @@ let csvToJson = require('convert-csv-to-json');
 let fileInputName = './movies/IMDb_movies.csv';
 let fileOutputName = './movies/IMDb_movies.json';
 
-csvToJson.fieldDelimiter(',').generateJsonFileFromCsv(fileInputName,fileOutputName);
+csvToJson.parseSubArray('"', ',').generateJsonFileFromCsv(fileInputName,fileOutputName);
